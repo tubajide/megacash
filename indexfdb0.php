2fbd64df767dc8d06d186495e1e4cf0abf45ab0a
Babajide Akinshola Israel <45528873+tubajide@users.noreply.github.com>
2020-04-28 11:13:17 +0100

    Add files via upload

---
+++ indexfdb0.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+	
+<!-- Mirrored from scfundz.com/index.php?page=4 by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 28 Apr 2020 08:00:32 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+		<meta name="viewport" content="width=device-width">
+		<title>Earn Cool cash</title>
+		<link rel="stylesheet" href="../use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="style.css">
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-164371506-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-164371506-1');
+		</script>
+	</head>
+	
+<body class="not-loged-in">
+	<header class="site-header">
+		<h1><a href="index.html">Scfundz</a></h1>
+
+		<div class="toggle-menu"><span></span></div>
+	</header>
+	<div class="site-sub-menu-wrapper">
+		<div class="site-sub-menu">
+			<a href="index.html"><i class="fa fa-home"></i><span>Home</span></a>
+			<a href="index.html"><i class="fas fa-newspaper"></i></i><span>Politics</span></a>
+			<a href="index.html"><i class="fas fa-star"></i><span>Entertainment</span></a>
+			<a href="index.html"><i class="far fa-futbol"></i><span>Sports</span></a>
+			<a href="index.html"><i class="fas fa-hospital-symbol"></i><span>Health</span></a>
+		</div>
+	</div>
+
+	<nav class="site-nav">
+		<div class="menu-header-menu-container">
+			<ul id="menu-header-menu" class="menu">
+				<li><a href="login.html">My account</a></li>
+				<li><a href="signup.html">Register</a></li>
+						<li><a href="post/coupon-distributors.html">coupon distributors</a></li>
+						<li><a href="post/top-5-earners.html">Top 5 earners</a></li>
+			</ul>
+	</div>
+</nav>
+	<!-- <div class="search-form"></div> -->
+
+	<script>
+		let toggleMenu = document.querySelector(".toggle-menu")
+		let navMenu = document.querySelector(".site-nav")
+		let navBox = document.querySelector(".site-nav ul")
+		let navLinks = document.querySelectorAll(".site-nav ul li a")
+		// let closeMenu = document.querySelector(".close-menu")
+
+		for (var i = 0; i < navLinks.length; i++) {
+
+		}
+
+		window.addEventListener('mouseup', function(event) {
+			if (event.target != navBox && event.target != navLinks[i] && event.target.parentNode != navBox) {
+					navMenu.style.display = "none"
+			}
+		})
+
+		toggleMenu.addEventListener('click', function(event) {
+			navMenu.style.display = "block"
+			// navMenu.style.opacity = "1"
+			// navMenu.style.zIndex = "9999"
+			// navBox.style.left = "0px"
+		})
+
+
+	</script>
+	<div class="site-wrapper"> <!-- wrapper -->
+		<!-- container-->
+		<div class="container">
+<div class="index-page">
+	<div class="post-section">
+		<h3>LATEST UPDATES</h3>	
+	</div>
+		
+			<article class="">
+				<div class="post-thumbnail">
+									</div>
+				<h2><a href="post/kano-covid-19-centre-closed-indefinitely-for-fumigation-after-staff-tested-positive.html">KANO COVID-19 CENTRE CLOSED INDEFINITELY FOR FUMIGATION AFTER STAFF TESTED POSITIVE </a></h2>
+			</article>
+		
+			<article class="">
+				<div class="post-thumbnail">
+									</div>
+				<h2><a href="post/---coronavirus-cases-rise-to-28-000-in-africa---------who-gives-update.html">“CORONAVIRUS CASES RISE TO 28,000 IN AFRICA ” – WHO GIVES UPDATE </a></h2>
+			</article>
+		
+			<article class="">
+				<div class="post-thumbnail">
+									</div>
+				<h2><a href="post/updates-on-withdrawal-today---.html">UPDATES ON WITHDRAWAL TODAY!!!</a></h2>
+			</article>
+		
+			<article class="">
+				<div class="post-thumbnail">
+									</div>
+				<h2><a href="post/i-only-started-making-money-as-an-actress-4-years-ago-----toyin-abraham.html">I ONLY STARTED MAKING MONEY AS AN ACTRESS 4 YEARS AGO – TOYIN ABRAHAM </a></h2>
+			</article>
+		
+			<article class="">
+				<div class="post-thumbnail">
+									</div>
+				<h2><a href="post/can-a-real-christian-contact-coronavirus--see-what-tb-joshua-has-to-say.html">CAN A REAL CHRISTIAN CONTACT CORONAVIRUS? SEE WHAT TB JOSHUA HAS TO SAY </a></h2>
+			</article>
+		
+			<article class="">
+				<div class="post-thumbnail">
+									</div>
+				<h2><a href="post/fifa-to-release---120m-to-members-in-fight-against-coronavirus.html">FIFA TO RELEASE £120m TO MEMBERS IN FIGHT AGAINST CORONAVIRUS </a></h2>
+			</article>
+		
+			<article class="">
+				<div class="post-thumbnail">
+									</div>
+				<h2><a href="post/first-patients-injected-in-uk-coronavirus-vaccine-trial.html">FIRST PATIENTS INJECTED IN UK CORONAVIRUS VACCINE TRIAL </a></h2>
+			</article>
+		</div>
+		
+<div class="pagination">	
+	<nav class="navigation pagination" role="navigation">
+		<div class="nav-links">
+							<a class="next page-numbers" href="index9ba9.html?page=3"><i class="fas fa-chevron-left"></i></a>
+					</div>
+	</nav>
+</div>
+
+
+
+		</div><!--  container  -->
+		<div class="side-bar">
+			<div class="footer-page-nav">
+	<h3>Pages</h3>
+	<div class="menu-pages-menu-container">
+		<ul id="menu-pages-menu" class="menu">
+								<li><a href="post/about-us.html">about us</a></li>
+								<li><a href="post/terms-and-conditions.html">terms and conditions</a></li>
+								<li><a href="post/contact-us.html">contact us</a></li>
+								<li><a href="post/privacy-policy.html">privacy policy</a></li>
+					</ul>
+	</div>
+</div>		</div>
+	</div> <!-- wrapper -->
+
+	<footer>
+		<div class="footer-credits">
+			<span>Copyright &copy; 2020 - Scfundz</span>
+		</div>
+
+		<div class="social-icons">
+			<a href="#"><i class="fab fa-facebook-f"></i></a>
+			<a href="#"><i class="fab fa-telegram"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+			<a href="#"><i class="fab fa-whatsapp"></i></a>
+		</div>
+
+		<div class="footer-nav"><nav class="site-nav">
+		<div class="menu-header-menu-container">
+			<ul id="menu-header-menu" class="menu">
+				<li><a href="login.html">My account</a></li>
+				<li><a href="signup.html">Register</a></li>
+						<li><a href="post/coupon-distributors.html">coupon distributors</a></li>
+						<li><a href="post/top-5-earners.html">Top 5 earners</a></li>
+			</ul>
+	</div>
+</nav></div>
+	</footer>
+</body>
+
+<!-- Mirrored from scfundz.com/index.php?page=4 by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 28 Apr 2020 08:00:43 GMT -->
+</html>
